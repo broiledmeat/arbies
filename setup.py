12d03edcd04b75dc99b36562866e4eb8a3421065
Derrick Staples <broiledmeat@gmail.com>
2023-02-06 09:50:05 -0500

    Add croniter to setup required packages.

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from distutils.core import setup
 setup(name='arbies',
       version='1.0',
       packages=['arbies'],
-      requires=['requests', 'Pillow', 'cairosvg', 'natsort', 'watchdog', 'toml'])
+      requires=['requests', 'Pillow', 'cairosvg', 'natsort', 'watchdog', 'toml', 'croniter'])
